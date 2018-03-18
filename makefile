06eef31ee84ba69e252ea50f38bdf6600fbd8c76
ronglll <971075271.qq.com>
2018-03-18 22:42:21 +0800

    second commit

--- makefile
+++ makefile
@@ -1,4 +1,7 @@
 main:test.o
 	cc -o test test.o
+	rm *.o
 test.o:test.c
-	cc -c test.c
+	cc -c test.c 
+clean: 
+	rm -f *.o test
--- test.c
+++ test.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 int main()
-{
-volatile int i=2,j;
+{ 
+int i=2,j;
 j=++i+(++i);
 printf("%d\n",j);
 return 0;
